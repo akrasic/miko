3e37e1f8fa4be37924d1fe0eec51bc2908b2a844
Antun Krasic <antun@martuna.co>
2013-06-06 04:13:40 +0200

    e107 support

---
+++ lib/scripts/e107.rb
@@ -0,0 +1,15 @@
+require 'miko/base.rb'
+module Miko 
+  class BBPress < Miko::Base
+
+    def initialize( path )
+      super( path )
+      
+      ## Load ver 
+      @version    = File.read( path )[/.*this->version.*/][/([\d.]+)/]
+      @acct_home   = path.gsub("bbpress.php", "")
+      @script      = "BBPress"
+    end
+    
+  end
+end
